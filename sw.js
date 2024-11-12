c79a8c17043aa51858c6896a1f9c0e6da8cc2a1e
2mLi <63608500+2mLi@users.noreply.github.com>
2024-11-12 00:31:52 +0000

    offline support

--- sw.js
+++ sw.js
@@ -1,18 +1,21 @@
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open('fuel-calc-v1').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html'
-            ]);
-        })
-    );
+const CACHE_NAME = 'fuel-calculator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 }); 
